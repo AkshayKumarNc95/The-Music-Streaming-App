b33054a6d4a4702fa25b172d320137b8a5c12dcf
akshaykumar95 <akshaykumarnc815@gmail.com>
2019-10-31 21:06:26 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/app',
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+    chunkFilename: '[id].js',
+    publicPath: '/',
+  },
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    stats: 'errors-only',
+    hot: true,
+    inline: true,
+    historyApiFallback: true,
+    port: 8000,
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+  },
+  resolve: {
+    extensions: ['.js', '.sass', '.json'],
+    modules: ['node_modules', 'app', 'seed'],
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+        }, {
+          loader: 'sass-loader',
+          options: {
+            includePaths: ['./app/styles'],
+          },
+        }],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-0', 'react'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/app/index.html',
+    }),
+    new ProgressBarPlugin(),
+  ],
+};
